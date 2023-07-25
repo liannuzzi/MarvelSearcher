92f9fcd0bda42c0f1cffcc4998b3d14159cfd86c
Lucas <iannuzzilucas@hotmail.com>
2023-07-25 00:16:38 -0300

    feat:favorites funcionality for comics

--- src/components/ComicModal/ComicModal.js
+++ src/components/ComicModal/ComicModal.js
@@ -1,8 +1,12 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 function ComicModal({ isOpen, onClose, heroName, heroId, comicList }) {
   if (!isOpen) return null;
+
+  const [favoriteComics, setFavoriteComics] = useState(
+    JSON.parse(localStorage.getItem("favoriteComics")) || []
+  );
 
   useEffect(() => {
     if (isOpen) {
@@ -10,6 +14,38 @@ function ComicModal({ isOpen, onClose, heroName, heroId, comicList }) {
       myModal.show();
     }
   }, [isOpen]);
+
+  // Fn que chequea si el comic esta en la lista de favoritos
+  const isFavoriteComic = (comicId) => {
+    return favoriteComics.some((comic) => comic.id === comicId);
+  };
+
+  // Function  para agregar/eliminar favoritos
+  const handleFavoriteToggle = (e, comicId) => {
+    e.stopPropagation();
+    const isComicInFavorites = favoriteComics.some(
+      (comic) => comic.id === comicId
+    );
+
+    if (isComicInFavorites) {
+      // Remueve comics de favoritos, filtr por todo lo que sea distinto al ID del comic
+      const updatedFavorites = favoriteComics.filter(
+        (comic) => comic.id !== comicId
+      );
+      setFavoriteComics(updatedFavorites);
+      localStorage.setItem("favoriteComics", JSON.stringify(updatedFavorites));
+    } else {
+      // Agrega comics a favoritos
+      const comicToAdd = comicList.find((comic) => comic.id === comicId);
+      if (comicToAdd) {
+        setFavoriteComics([...favoriteComics, comicToAdd]);
+        localStorage.setItem(
+          "favoriteComics",
+          JSON.stringify([...favoriteComics, comicToAdd])
+        );
+      }
+    }
+  };
 
   return (
     <div>
@@ -31,31 +67,41 @@ function ComicModal({ isOpen, onClose, heroName, heroId, comicList }) {
                 <ul className="modal-comic-list">
                   {comicList.map((comic) => {
                     return (
-                      <Link
-                        href={`/comics/${comic.id}`}
-                        style={{ color: "inherit", textDecoration: "inherit" }}
-                      >
-                        <li
-                          className="modal-comic-item"
-                          key={comic.id}
-                          data-bs-dismiss="modal"
-                          style={{ cursor: "pointer" }}
+                      <li className="modal-comic-item" key={comic.id}>
+                        <Link
+                          href={`/comics/${comic.id}`}
+                          style={{
+                            color: "inherit",
+                            textDecoration: "inherit",
+                          }}
                         >
                           <div>
                             <img
                               className="modal-comic-img"
                               src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
                               alt={comic.title}
+                              data-bs-dismiss="modal"
                             ></img>
                           </div>
-                          <div className="modal-comic-information">
-                            <p className="modal-comic-title">{comic.title}</p>
-                            <p className="modal-comic-description">
-                              {comic.description}
-                            </p>
-                          </div>
-                        </li>
-                      </Link>
+                        </Link>
+                        <div className="modal-comic-information">
+                          <p className="modal-comic-title">
+                            {comic.title}
+                            <i
+                              className={`bi bi-star${
+                                isFavoriteComic(comic.id)
+                                  ? "-fill text-warning"
+                                  : ""
+                              }`}
+                              onClick={(e) => handleFavoriteToggle(e, comic.id)}
+                              style={{ cursor: "pointer", marginLeft: "5px" }}
+                            ></i>
+                          </p>
+                          <p className="modal-comic-description">
+                            {comic.description}
+                          </p>
+                        </div>
+                      </li>
                     );
                   })}
                 </ul>
