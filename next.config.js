16d649007170b656a776a38aad9fe463c2c595bd
Lucas <iannuzzilucas@hotmail.com>
2023-07-30 20:47:30 -0300

    chore:test env file

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    NEXT_PUBLIC_API_KEY: "b801ecfcc69a63855fa551806cf20296",
+    NEXT_PUBLIC_TOKEN_HASH: "1df3d49570e36411d700b0edf856d649",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
